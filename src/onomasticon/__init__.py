d428499b7f3caaffbc7209d65672397ce5da57de
s.rincon <rincorpes@gmail.com>
2026-08-03 14:10:43 -0500

    feat(studio): enhance version retrieval logic and add tests for Studio package

--- src/onomasticon/__init__.py
+++ src/onomasticon/__init__.py
@@ -2,12 +2,9 @@
 Public package exports for onomasticon.
 """
 
-from ._version import __version__, get_version
 from .onomasticon import ImplementationRegistry, ImplementationT
 
 __all__ = [
     "ImplementationRegistry",
     "ImplementationT",
-    "__version__",
-    "get_version",
 ]
